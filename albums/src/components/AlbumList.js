cb4cbefc335502b1288b0e82f953bee54e3677ab
Josue Chinchilla <josue.chinchilla@prodigious.com>
2017-04-12 09:14:33 -0600

    initial commit

---
+++ albums/src/components/AlbumList.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import axios from 'axios';
+
+class AlbumList extends Component {
+    state = {
+        albums: []
+    };
+
+    componentWillMount() {
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => this.setState({albums : response.data}));
+    }
+
+    renderAlbums(){
+        return this.state.albums.map(album => <Text>{album.title}</Text>);
+    }
+
+    render() {
+        console.log(this.state);
+        return (
+            <View>
+                {this.renderAlbums()}
+            </View>
+        );
+    }
+}
+
+export default AlbumList;
